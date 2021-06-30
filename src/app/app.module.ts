edb8f03e926f7f606633909ce3b18d834b1012de
Seulgi Jo <sylivajo172@gmail.com>
2021-06-30 16:06:37 +0200

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AppRoutingModule } from './app-routing/app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -10,16 +10,18 @@ import { MatCardModule} from '@angular/material/card';
 import { MatButtonModule} from '@angular/material/button';
 
 import { DatePipe } from '@angular/common';
-import { AppRoutingModule } from './app-routing.module';
+import { DishService} from './services/dish.service';
+
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
-
-import 'hammerjs';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
-
-import { DishService} from './services/dish.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+import 'hammerjs';
 
 
 @NgModule({
@@ -28,7 +30,10 @@ import { FooterComponent } from './footer/footer.component';
     MenuComponent,
     DishdetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
