9560d14ec9f30b194bbf723e61dbbcbbcc262496
Seulgi Jo <sylviajo172@gmail.com>
2021-07-16 17:14:30 +0200

    Attribute Directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -41,6 +41,7 @@ import 'hammerjs';
 import { LoginComponent } from './login/login.component';
 
 import { baseURL } from './shared/baseurl';
+import { HighlightDirective } from './directives/highlight.directive';
 
 
 @NgModule({
@@ -53,7 +54,8 @@ import { baseURL } from './shared/baseurl';
     HomeComponent,
     AboutComponent,
     ContactComponent,
-    LoginComponent
+    LoginComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
