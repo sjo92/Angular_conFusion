21c89eed7500980b09173febd2c89e1b022f5a55
Seulgi Jo <sylviajo172@gmail.com>
2021-07-16 19:11:26 +0200

    Angular Animations Part2

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,21 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { baseURL } from '../shared/baseurl';
 import { Leader } from '../shared/leader';
 import { LeaderService } from '../services/leader.service';
+import { visibility, flyInOut } from '../animations/app.animation';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
+  host: {
+    '[@flyInOut]': 'true',
+    'style': 'display: block;'
+  },
+  animations: [
+    visibility(), flyInOut()
+  ]
 })
 export class AboutComponent implements OnInit {
 
   leaders: Leader[]
+  BaseURL;
 
-  constructor(private leaderService: LeaderService) { }
+  constructor(private leaderService: LeaderService) { 
+    this.BaseURL = baseURL
+  }
 
   ngOnInit(): void {
     this.leaderService.getLeaders()
-    .subscribe((leaders) => this.leaders=leaders );
+    .subscribe((leaders) => this.leaders=leaders);
   }
-
 }
--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -4,18 +4,18 @@ import { Observable, of } from 'rxjs';
 import { delay } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 import { baseURL } from '../shared/baseurl';
-import { map } from 'rxjs/operators';
-
+import { map, catchError } from 'rxjs/operators';
+import { ProcessHTTPMsgService } from './process-httpmsg.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LeaderService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private processHTTPMsgService: ProcessHTTPMsgService) { }
 
   getLeaders() : Observable<Leader[]> {
-    return this.http.get<Leader[]>(baseURL + 'leadership');
+    return this.http.get<Leader[]>(baseURL + 'leadership').pipe(catchError(this.processHTTPMsgService.handleError));
   }
 
   getLeader(id: string) : Observable<Leader> {
